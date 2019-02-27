02f01b0ac846cc1ae29a40682ee6f5a5b69c5d3c
indhumathidevaraj <47887347+indhumathidevaraj@users.noreply.github.com>
2019-02-27 15:39:13 +0530

    Create set1_1.c

---
+++ set1_1.c
@@ -0,0 +1,24 @@
+# cprogram
+#include <stdio.h>
+
+int main()
+{
+    int n;
+    printf("enter the number");
+    scanf("%d",&n);
+    if(n>0)
+    {
+        printf("positive");
+    }
+else if(n<0)
+{
+    printf("negative");
+}
+else
+{
+    printf("equal to zero");
+}
+
+
+    return 0;
+}
