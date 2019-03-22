63eea1052d80ffbb6f6f192d7b0215c3a3419017
indhumathidevaraj <47887347+indhumathidevaraj@users.noreply.github.com>
2019-03-22 15:41:38 +0530

    Create pset1_5.c

---
+++ pset1_5.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main()
+
+{
+char a[40];
+int i,c=0;
+scanf("%c",&a);
+for(i=0;i<a[i];i++)
+{
+for(i=0;i<a[i];i++)
+{
+if(a[i]=='V')
+c=c+5;
+else if(a[i]=='I')
+c=c+1;
+else if(a[i]=='X')
+c=c+10;
+}
+printf("%d\n",c);
+
+    return 0;
+}
+}
